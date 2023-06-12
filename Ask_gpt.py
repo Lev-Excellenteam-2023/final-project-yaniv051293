39d4dcb3ee64140e9801189279da5081b76ba11d
yaniv051293 <yaniv051293@gmail.com>
2023-06-12 15:30:56 +0300

    adding asyncio and fix notes

--- Ask_gpt.py
+++ Ask_gpt.py
@@ -1,36 +1,35 @@
-import time
 import openai
-from openai import APIError
+import aiohttp
 
-conversation = []
-conversation.append(
-    {"role": "system", "content": f"you are a helpful AI that is explaining a PowerPoint presentation."})
-openai.api_key = "sk-"# enter your key
-max_retries = 3
-retry_delay = 20  # Delay in seconds between retries
+conversation = [{"role": "system", "content": f"you are a helpful AI that is explaining a PowerPoint presentation."}]
+openai.api_key = "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"  # enter your key
 
 
-def explain_page(page: str):
-    request = "Give me a brief explanation of this page, " \
-              "Write the explanation as a short paragraph from an article:\n" + page
-    return send_gpt_request(request)
+async def send_gpt_request(user_message: str):
+    """
+    Sends a request to the GPT-3.5-turbo model for generating text based on a user message.
 
+    :param user_message: The message from the user to be used as a prompt for text generation.
+    :return: A dictionary containing the response from the GPT-3.5-turbo model.
+    """
+    # Set engine and parameters
+    engine = "gpt-3.5-turbo"
+    max_tokens = 512
 
-def send_gpt_request(user_message:str)->str:
-    conversation.append({"role": "user", "content": user_message})
-    for retry in range(max_retries):
-        try:
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
-            assistant_reply = response['choices'][0]['message']['content']
-            conversation.append({"role": "assistant", "content": assistant_reply})
-            return assistant_reply
-        except APIError as e:
-            error_message = e.message
-            # Handle the exception, e.g., log the error or display an error message
-            print(f"API Error: {error_message}")
-            print(f"Retrying... (attempt {retry+1}/{max_retries})")
-            time.sleep(retry_delay)
+    # Generate text from prompt asynchronously
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(
+            "https://api.openai.com/v1/chat/completions",  # Updated endpoint URL
+            headers={
+                "Authorization": f"Bearer {openai.api_key}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "messages": [{"role": "system", "content": "You are a helpful assistant."},
+                             {"role": "user", "content": user_message}],
+                "max_tokens": max_tokens,
+                "model": engine  # Add the model parameter
+            }
+        )
+        return await response.json()
 
-    # If maximum retries reached without success, handle the failure
-    print("Request failed after maximum retries")
-    # Perform any necessary actions, such as displaying an error message or terminating the program
